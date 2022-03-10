d37ec05341e2fc6e62e4a0ee91dda31be84f4108
BKE <baekee@naver.com>
2022-03-10 17:39:56 +0900

    스프링 부트 입문_김영한

---
+++ src/main/java/com/practice/spring_practice/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.practice.spring_practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data","반가워!!");
+        return "hello";
+    }
+}
